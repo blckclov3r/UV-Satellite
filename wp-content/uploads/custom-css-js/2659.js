ec102fa62c88e7d6017dac9376183b0e42350693
aljun.abrenica@mindweb.com.ph <blckclov3r@gmail.com>
2021-03-21 20:48:30 -0700

    add css/js minimal protection

---
+++ wp-content/uploads/custom-css-js/2659.js
@@ -0,0 +1,14 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+jQuery(document).ready(function( $ ){
+     $("body").on("contextmenu", "img", function(e) {
+        return false;
+     });
+});
+
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
