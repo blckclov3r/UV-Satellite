84ecb9f062e10a2892661375a229adad7915bbfe
aljun.abrenica@mindweb.com.ph <blckclov3r@gmail.com>
2021-03-22 23:50:15 -0700

    image not show

---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,2 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$environment_variable = '{"theme":{"folder_name":"kadence","mapped_name":"uvsatellite"},"allowed_paths":["C:\/xampp\/htdocs\/uvwp\/wp-content\/themes"],"cache_path":"C:\/xampp\/htdocs\/uvwp\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/uvwp\/"}'; ?>
